5622735e6784e4cb3f06bfa10dd464ee0d987a78
Ehsan <ehsan.tarokh@gmail.com>
2026-07-13 14:09:57 -0700

    Refactor collector to use SMART data provider

--- src/collector.py
+++ src/collector.py
@@ -4,11 +4,21 @@ from discovery import discover_disks
 from parser import parse_smart_data
 
 
+def get_smart_json():
+    """
+    Load SMART data from a sample JSON file.
+
+    This function will later be replaced with a real smartctl call,
+    but the rest of the collector will not need to change.
+    """
+    with open("samples/sata-hdd.json", "r", encoding="utf-8") as file:
+        return json.load(file)
+
+
 def collect():
     disks = discover_disks("samples/lsblk.json")
 
-    with open("samples/sata-hdd.json", "r", encoding="utf-8") as file:
-        smart_data = json.load(file)
+    smart_data = get_smart_json()
 
     parsed_smart = parse_smart_data(smart_data)
 
